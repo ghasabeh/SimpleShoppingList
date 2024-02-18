b97ca188d084eb5fbdaf0dc3c31f4d8b86a79e50
Alireza Ghasabeie <alireza.ghasabeie@devolon.fi>
2024-02-18 18:05:45 +0330

    create separate kotlin file.
    add composable function to draw button and data class item and lazy column to add to it later.

--- app/src/main/java/com/ghasabeie/simpleshoppinglist/MainActivity.kt
+++ app/src/main/java/com/ghasabeie/simpleshoppinglist/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ShoppingListApp()
                 }
             }
         }
---
+++ app/src/main/java/com/ghasabeie/simpleshoppinglist/ShoppingListApp.kt
@@ -0,0 +1,49 @@
+package com.ghasabeie.simpleshoppinglist
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
+)
+
+@Composable
+fun ShoppingListApp() {
+    val shoppingListItem by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = { /*TODO*/ }) {
+            Text(text = "Add Item")
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(shoppingListItem) {
+
+            }
+        }
+    }
+}
