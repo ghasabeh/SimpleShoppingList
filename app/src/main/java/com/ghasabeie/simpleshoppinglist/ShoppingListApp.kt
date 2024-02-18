6e29a64137320ec3f76aa9d7d6468549175d1463
Alireza Ghasabeie <alireza.ghasabeie@devolon.fi>
2024-02-18 19:20:30 +0330

    add raw alert dialog to display when clicking on add item button and hide it on dismiss it.

--- app/src/main/java/com/ghasabeie/simpleshoppinglist/ShoppingListApp.kt
+++ app/src/main/java/com/ghasabeie/simpleshoppinglist/ShoppingListApp.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,16 +26,18 @@ data class ShoppingItem(
     var isEditing: Boolean = false
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp() {
-    val shoppingListItem by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var shoppingListItem by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
             modifier = Modifier.align(Alignment.CenterHorizontally),
-            onClick = { /*TODO*/ }) {
+            onClick = { showDialog = true }) {
             Text(text = "Add Item")
         }
         LazyColumn(
@@ -43,6 +47,11 @@ fun ShoppingListApp() {
         ) {
             items(shoppingListItem) {
 
+            }
+        }
+        if (showDialog) {
+            AlertDialog( onDismissRequest = { showDialog = false }) {
+                Text(text = "I am in alert Dialog!")
             }
         }
     }
